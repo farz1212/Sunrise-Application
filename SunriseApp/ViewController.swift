233cda9bb3ca391c883d05c2dacbf822b4ef49f5
farzaad <fgoiporia@gmail.com>
2020-08-25 19:52:21 -0400

    Initial Commit

---
+++ SunriseApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SunriseApp
+//
+//  Created by Farzaad Goiporia on 2020-08-25.
+//  Copyright © 2020 Farzaad Goiporia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
